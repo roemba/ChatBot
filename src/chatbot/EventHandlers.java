e7c32f0674bf8051f07609677297db3d530ddd9e
roemba <student@ROEMER-HENDRIKX-GRADE-12.local>
2013-09-06 21:13:42 +0200

    Added lightning projectile

--- src/chatbot/EventHandlers.java
+++ src/chatbot/EventHandlers.java
@@ -4,19 +4,17 @@
  */
 package chatbot;
 import org.bukkit.Bukkit;
+import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.World;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Explosive;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Snowball;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.scheduler.BukkitTask;
 
 /**
  *
@@ -42,33 +40,33 @@ public class EventHandlers implements Listener {
     }
     
     @EventHandler
-    public void BlazeRodShooter(PlayerInteractEvent event){
+    public void PlayerInteract(PlayerInteractEvent event){
            
         if (event.getPlayer().getItemInHand().getType().equals(Material.BLAZE_ROD)){
             
-            ProjectileThread runnable = new ProjectileThread(event);
-            //Thread thread_object = new Thread(runnable);
-            //thread_object.start();
-            /*Player player = event.getPlayer();
+            Player player = event.getPlayer();
             
-                        //Spawnt net voor de player
-                        Location proj = player.getEyeLocation().toVector().add(player.getLocation().getDirection().multiply(2)).toLocation(player.getWorld(), player.getLocation().getYaw(), player.getLocation().getPitch());
-                        Snowball snow = player.getWorld().spawn(proj, Snowball.class);
+            //Spawnt net voor de player
+            Location proj = player.getEyeLocation().toVector().add(player.getLocation().getDirection().multiply(2)).toLocation(player.getWorld(), player.getLocation().getYaw(), player.getLocation().getPitch());
+            Snowball snow = player.getWorld().spawn(proj, Snowball.class);
             
-                        snow.setShooter(player);
-                        snow.setVelocity(player.getEyeLocation().getDirection().multiply(2));
-            */
-        
-            
-            
-            
-            
-            
-            
-            
+            snow.setShooter(player);
+            snow.setVelocity(player.getEyeLocation().getDirection().multiply(2));
+            player.getWorld().playEffect(proj, Effect.SMOKE, 2);
             
             
         }
+    }
+    
+    @EventHandler
+    public void ProjectileHit(ProjectileHitEvent event){
+        if (event.getEntityType().equals(EntityType.SNOWBALL)){
+            event.getEntity().getWorld().createExplosion(event.getEntity().getLocation(), 4F, true);
+            event.getEntity().getWorld().strikeLightning(event.getEntity().getLocation());
+        
+    }
+    
+    
     }
 }
 
